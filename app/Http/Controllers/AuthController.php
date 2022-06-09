b33b9e000ebaefb979c7c0ab7cafad2f7b51f830
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-08 22:33:49 -0600

    Add Laravel with very basic auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+class AuthController extends Controller
+{
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'name' => ['bail', 'required', 'max:255'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            return response("");
+        }
+
+        return abort(403);
+
+    }
+}
