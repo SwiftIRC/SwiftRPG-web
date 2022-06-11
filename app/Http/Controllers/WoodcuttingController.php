38078602c460e843c9f8474dcccf515a47cbcacf
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-11 09:33:07 -0600

    Attempting to limit chop() to once a minute

--- app/Http/Controllers/WoodcuttingController.php
+++ app/Http/Controllers/WoodcuttingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Item;
 use App\Models\Inventory;
+use App\Models\CommandLog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -16,6 +17,11 @@ class WoodcuttingController extends Controller
 
     public function chop()
     {
+        $last_run = CommandLog::where('user_id', Auth::id())->where('command', 'chop')->where('created_at', '>=', now()->subMinutes(1))->get()->count();
+        if ($last_run > 0) {
+            return response()->json(['error' => 'You can only run this command once every minute.'], 403);
+        }
+
         $user = Auth::user();
         $user->woodcutting += 5;
         $user->save();
@@ -27,6 +33,14 @@ class WoodcuttingController extends Controller
 
         $logs = count($inventory->items()->where('name', 'Logs')->get()->all());
 
-        return response()->json(['woodcutting' => $user->woodcutting, 'logs' => $logs]);
+        $output = ['woodcutting' => $user->woodcutting, 'logs' => $logs];
+
+        CommandLog::create([
+            'user_id' => Auth::user()->id,
+            'command' => 'chop',
+            'message' => json_encode($output),
+        ]);
+
+        return response()->json($output);
     }
 }
