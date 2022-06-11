2f1980ff9d43f36456ce1878e25c1afd7415abf4
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-11 00:27:04 -0600

    Untracked files

---
+++ database/migrations/2022_06_11_061729_create_command_logs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('command_logs', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger("user_id")->unsigned();
+
+            $table->string('command', 100);
+            $table->string('message', 255);
+
+            $table->timestamps();
+
+            $table->foreign("user_id")->references("id")->on("users");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+};
