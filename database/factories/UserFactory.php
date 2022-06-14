9f7a06dfbd4efad9cd273d1ea1d4ff8612af25b0
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-14 13:59:04 -0600

    API for user location

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,6 +21,8 @@ class UserFactory extends Factory
             'name' => $this->faker->userName(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'x' => '0',
+            'y' => '0',
         ];
     }
 }
