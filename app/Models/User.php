22bc40e9030b1b2cd6576d60414f23a9b0582602
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-16 20:39:30 -0600

    fix: Databases are hard

--- app/Models/User.php
+++ app/Models/User.php
@@ -94,7 +94,7 @@ class User extends Authenticatable
 
     public function building()
     {
-        return $this->belongsTo(Building::class);
+        return $this->hasMany(Building::class, 'id', 'building_id')->first();
     }
 
     public function tile()
