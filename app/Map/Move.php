137cd5a27a6390552174fc1d169e542dad5fc2f6
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-18 12:44:25 -0600

    Counting internal tiles

--- app/Map/Move.php
+++ app/Map/Move.php
@@ -36,6 +36,36 @@ class Move
         return Tile::where('x', $x)->where('y', $y)->first();
     }
 
+    public function get_adjacent_missing_tile(Tile $tile, string $direction)
+    {
+        $x = $tile->x;
+        $y = $tile->y;
+
+        switch ($direction) {
+            case 'north':
+                $y++;
+                break;
+            case 'east':
+                $x++;
+                break;
+            case 'south':
+                $y--;
+                break;
+            case 'west':
+                $x--;
+                break;
+        }
+
+        if ($tile = Tile::where('x', $x)->where('y', $y)->first()) {
+            return;
+        }
+        $adjacent_tile = new Tile();
+        $adjacent_tile->x = $x;
+        $adjacent_tile->y = $y;
+
+        return $adjacent_tile;
+    }
+
     public function get_adjacent_edge(Tile $tile, string $direction)
     {
         $x = $tile->x;
@@ -77,99 +107,110 @@ class Move
     {
         $edge = $this->get_adjacent_edge($tile, $direction);
 
-        return $edge ? $edge->pivot->is_road : false;
+        return $edge ? $edge->pivot->is_road : $edge;
     }
 
-    public function fill_in_isolated_tiles(Tile $tile)
+    public function fill_in_missing_tiles_if_isolated()
     {
-        $missing_tiles = $this->find_missing_tiles_from_border($tile);
+        $tiles = Tile::all();
+        $tile_coords = [];
+        $invalid_coords = [];
+        $max_x = 0;
+        $min_x = 0;
+        $max_y = 0;
+        $min_y = 0;
 
-        foreach ($missing_tiles as $missing_tile) {
-            $this->fill_in_missing_tile_if_isolated($tile, $missing_tile);
+        foreach ($tiles as $tile) {
+            $tile_coords[$tile->x][$tile->y] = $tile;
+
+            if ($tile->x > $max_x) {
+                $max_x = $tile->x;
+            } elseif ($tile->x < $min_x) {
+                $min_x = $tile->x;
+            }
+            if ($tile->y > $max_y) {
+                $max_y = $tile->y;
+            } elseif ($tile->y < $min_y) {
+                $min_y = $tile->y;
+            }
         }
+
+        $max_x++;
+        $min_x--;
+        $max_y++;
+        $min_y--;
+
+        for ($x = $min_x; $x <= $max_x; $x++) {
+            $invalid_coords[$x][$max_y] = true;
+            $invalid_coords[$x][$min_y] = true;
+        }
+        for ($y = $min_y; $y <= $max_y; $y++) {
+            $invalid_coords[$max_x][$y] = true;
+            $invalid_coords[$min_x][$y] = true;
+        }
+
+        $empty_tiles = $this->find_all_empty_tiles($max_x, $max_y, $min_x, $min_y, $tile_coords);
+
+        $external_empty_tiles = $this->find_all_connected_empty_tiles($empty_tiles, $tile_coords);
+        $internal_tiles = array_diff($empty_tiles, $external_empty_tiles);
+
+        info("Internal tiles: " . count($internal_tiles));
     }
 
-    public function fill_in_missing_tile_if_isolated(Tile $tile, string $missing_direction)
+    public function find_all_empty_tiles(int $max_x, int $max_y, int $min_x, int $min_y, array $tile_coords)
     {
-        $missing_x = $tile->x;
-        $missing_y = $tile->y;
+        $empty_tiles = [];
+        for ($x = $min_x; $x <= $max_x; $x++) {
+            for ($y = $min_y; $y <= $max_y; $y++) {
+                if (!isset($tile_coords[$x][$y])) {
+                    $tile = new Tile();
+                    $tile->x = $x;
+                    $tile->y = $y;
 
-        switch ($missing_direction) {
-            case 'north':
-                $missing_y++;
-                break;
-            case 'east':
-                $missing_x++;
-                break;
-            case 'south':
-                $missing_y--;
-                break;
-            case 'west':
-                $missing_x--;
-                break;
+                    $empty_tiles[] = $tile;
+                }
+            }
+        }
+        return $empty_tiles;
+    }
+
+    public function find_all_connected_empty_tiles(array $empty_tiles, array $tile_coords): array
+    {
+        $connected_tiles = [];
+
+        $search_tile = $empty_tiles[count($empty_tiles) - 1];
+
+        return $this->iterate_over_all_connected_empty_tiles($search_tile, $empty_tiles, $connected_tiles);
+    }
+
+    public function iterate_over_all_connected_empty_tiles(Tile $search_tile, array $empty_tiles, array $connected_tiles): array
+    {
+        $tiles = $this->get_adjacent_missing_tiles($search_tile, $empty_tiles, $connected_tiles);
+        if (!empty($tiles)) {
+            $connected_tiles = array_merge($connected_tiles, $tiles);
+
+            foreach ($tiles as $tile) {
+                $connected_tiles = $this->iterate_over_all_connected_empty_tiles($tile, $empty_tiles, $connected_tiles);
+            }
         }
 
+        return $connected_tiles;
+    }
+
+    public function get_adjacent_missing_tiles(Tile $tile, array $empty_tiles, array $connected_tiles): array
+    {
         $directions = ['north', 'east', 'south', 'west'];
-
-        $inverted_directions = [
-            'north' => 'south',
-            'east' => 'west',
-            'south' => 'north',
-            'west' => 'east',
-        ];
+        $tiles = [];
 
         foreach ($directions as $direction) {
-            $inverted_direction = $inverted_directions[$direction];
-            $x = $missing_x;
-            $y = $missing_y;
-
-            switch ($direction) {
-                case 'north':
-                    $y++;
-                    break;
-                case 'east':
-                    $x++;
-                    break;
-                case 'south':
-                    $y--;
-                    break;
-                case 'west':
-                    $x--;
-                    break;
-            }
-
-            $adjacent_tile = Tile::where('x', $x)->where('y', $y)->first();
-
-            if (!$adjacent_tile) {
-                return;
-            }
-            $adjacent_edge = $adjacent_tile->edges()->where('direction', $inverted_direction)->first();
-
-            if ($adjacent_edge->pivot->is_road) {
-                return;
+            $adjacent_tile = $this->get_adjacent_missing_tile($tile, $direction);
+            if ($adjacent_tile && in_array($adjacent_tile, $empty_tiles) && !in_array($adjacent_tile, $connected_tiles)) {
+                $tiles[] = $adjacent_tile;
             }
         }
 
-        Tile::create([
-            'x' => $missing_x,
-            'y' => $missing_y,
-            'psuedo_id' => $missing_x . ',' . $missing_y,
-        ]);
-    }
 
-    public function find_missing_tiles_from_border(Tile $tile)
-    {
-        $missing_directions = [];
-        $directions = ['north', 'east', 'south', 'west'];
-
-        foreach ($directions as $direction) {
-            $adjacent_tile = $this->get_adjacent_tile($tile, $direction);
-            if (!$adjacent_tile) {
-                $missing_directions[] = $direction;
-            }
-        }
-
-        return $missing_directions;
+        return $tiles;
     }
 
     public function move(User $user, string $direction)
@@ -247,7 +288,7 @@ class Move
         $user->tile_id = $new_tile->id;
         $user->save();
 
-        $this->fill_in_isolated_tiles($new_tile);
+        $this->fill_in_missing_tiles_if_isolated();
 
         return $new_tile;
     }
