e2293e8623dd4c8a4107ec4ebbf20fbcb7daeb34
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-18 13:45:12 -0600

    Fill in isolated tiles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,19 +17,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
         $this->call([
             TerrainSeeder::class,
             EdgeSeeder::class,
             TileSeeder::class,
             NpcSeeder::class,
-        ]);
-        $this->call([
             UserSeeder::class,
             ItemSeeder::class,
         ]);
