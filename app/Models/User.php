b028bbe508213e839fe8eb5f410521eb194b1b60
Ryan William O'Hara <ryan@ryanwohara.com>
2022-06-20 16:55:07 -0600

    feat: Ability to fail pickpocketing

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,8 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'password',
+        'hitpoints',
+        'mana',
         'thieving',
         'woodcutting',
         'tile_id',
