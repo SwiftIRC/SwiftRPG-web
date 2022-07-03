50b9454f4063c1ec98ef566325deb67eda972734
Ryan William O'Hara <ryan@ryanwohara.com>
2022-07-03 12:16:28 -0600

    fix: Build npm dependencies in GH Action

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-// import react from '@vitejs/plugin-react';
-// import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -9,14 +7,5 @@ export default defineConfig({
             'resources/css/app.css',
             'resources/js/app.js',
         ]),
-        // react(),
-        // vue({
-        //     template: {
-        //         transformAssetUrls: {
-        //             base: null,
-        //             includeAbsolute: false,
-        //         },
-        //     },
-        // }),
     ],
 });
