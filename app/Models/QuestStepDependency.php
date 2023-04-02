a7dc15ce3e5bed6d2fad6f7f0193719cd3a4c937
Ryan William O'Hara <ryan@ryanwohara.com>
2023-04-02 12:05:13 -0600

    chore: Remove unreachable code

--- app/Models/QuestStepDependency.php
+++ app/Models/QuestStepDependency.php
@@ -45,11 +45,5 @@ class QuestStepDependency extends Model
     {
         return $this->doesntHave('completeSteps')
             ->where('quest_id', $quest_id);
-        dd($this->completeSteps()->get()->pluck('quest_step_id')->toArray());
-        return $this->hasOne(CompletedQuestStep::class, 'quest_step_id', 'quest_step_id')
-            ->selectRaw("quest_step_dependencies.*, completed_quest_steps.*, quest_step_dependencies.id as quest_step_dependency_id, completed_quest_steps.id as completed_quest_step_id")
-            ->leftJoin('quest_step_dependencies', 'quest_step_dependencies.quest_step_dependency_id', '=', 'quest_step_dependencies.quest_step_id')
-            ->whereNotNull('quest_step_dependencies.id')
-            ->whereNotIn('quest_step_id', $this->completeSteps()->get()->pluck('quest_step_id')->toArray());
     }
 }
