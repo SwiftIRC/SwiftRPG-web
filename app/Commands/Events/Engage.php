d399ce6d59f84bbcb355c46db5a707d5e1a89a74
Ryan William O'Hara <ryan@ryanwohara.com>
2023-04-23 20:32:24 -0600

    wip: Event engage

--- app/Commands/Events/Engage.php
+++ app/Commands/Events/Engage.php
@@ -3,25 +3,24 @@
 namespace App\Commands\Events;
 
 use App\Commands\Command;
+use App\Models\Event;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class Engage extends Command
 {
-    public function queue(array $input = []): \Illuminate\Http\Response
+    public function queue(array $input = []): Response
     {
         $this->user = Auth::user();
+        $this->command = array_pop($input);
 
-        // $event = Event::withTrashed()->firstWhere('deleted_at', '>', now());
-        // if (empty($event)) {
-        //     return response()->object(['failure' => 'No active event.', 'ticks' => 0], 400);
-        // }
-
-        // $event->reward->skills()->syncWithoutDetaching($request->skills);
-        // $event->reward->items()->syncWithoutDetaching($request->items);
-
-        // return response()->json(['success' => 'Event completed.']);
+        $event = Event::withTrashed()->firstWhere('deleted_at', '>', now());
+        if (empty($event)) {
+            return response()->object(['command' => $this->command, 'failure' => 'No active event.', 'ticks' => 0], 400);
+        }
 
         return response()->object([
+            'command' => $this->command,
             'reward' => $this->generateReward(),
             'ticks' => $this->command->ticks,
         ]);
